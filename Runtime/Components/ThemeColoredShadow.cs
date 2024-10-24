d4140dadedb82d3543973c079f9ae5787e8d509a
Andrey Lemin <anlemin@mail.ru>
2024-10-24 17:27:50 +0300

    Add ThemeColoredShadow

---
+++ Runtime/Components/ThemeColoredShadow.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DoubleY.ColorThemes.Components
+{
+    [RequireComponent(typeof(Shadow))]
+    public class ThemeColoredShadow : ThemeSingleColoredComponent
+    {
+        [SerializeField]
+        private Shadow _shadow;
+
+        public override Component GetTargetComponent() => _shadow;
+
+        // called only in Editor
+        private void OnValidate()
+        {
+            _shadow ??= GetComponent<Shadow>();
+        }
+
+        protected override Color GetColor() => _shadow.effectColor;
+        protected override void SetColor(Color color) => _shadow.effectColor = color;
+    }
+}
